dd19df8aadcb970b3b1e428e677ed22ffa0780b5
Dmitry <wodi220500@gmail.com>
2021-04-05 19:55:52 +0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Dmitriy Chernov on 05.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
